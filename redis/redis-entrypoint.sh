b90bd701c437bec67f78f4ff871fc83bd3ac95b8
khoanguyen2610 <anhkhoa.nguyen2610@gmail.com>
2019-05-13 17:53:01 +0700

    last day push

--- redis/redis-entrypoint.sh
+++ redis/redis-entrypoint.sh
@@ -7,17 +7,18 @@ pidfile "var/run/sentinel.pid"
 dir "/var/lib/redis/sentinel"
 
 sentinel monitor docker-cluster $MASTER_HOST 6379 $SENTINEL_QUORUM
+sentinel auth-pass docker-cluster redis-auth
 sentinel down-after-milliseconds docker-cluster $SENTINEL_DOWN_AFTER
 sentinel parallel-syncs docker-cluster 1
 sentinel failover-timeout docker-cluster $SENTINEL_FAILOVER
 EOF
-
 redis-sentinel $REDIS_DIR/sentinel.conf
 
-if [ "$IS_SLAVE" == true ]; then
-	redis-server $REDIS_DIR/redis.conf --slaveof $MASTER_HOST 6379 \
-        --loadmodule $REDIS_MODULE_DIR/redisjson-master/src/rejson.so
+if [ "$IS_SLAVE" = true ]; then
+    redis-server $REDIS_DIR/redis.conf --slaveof $MASTER_HOST 6379
 else
-    redis-server $REDIS_DIR/redis.conf \
-        --loadmodule $REDIS_MODULE_DIR/redisjson-master/src/rejson.so
+    redis-server $REDIS_DIR/redis.conf
 fi
+
+
+
